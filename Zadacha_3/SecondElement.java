92babc21623ced4972fd75409170c9187e042f9c
Evgen.Batuev <Evgen.Batuev@x5.ru>
2025-06-18 19:35:01 +0300

    Задача 3

---
+++ Zadacha_3/SecondElement.java
@@ -0,0 +1,20 @@
+package Zadacha_3;
+
+public class SecondElement {
+    public static int findSecondLargest(int[] array) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : array) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num < largest) {
+                secondLargest = num;
+            }
+        }
+
+        return secondLargest;
+    }
+}
+
